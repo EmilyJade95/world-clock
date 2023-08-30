a6fe74388c0bf2a3e31e8464f3521e21c909fe32
Emily <emilyjharrison1@gmail.com>
2023-08-30 18:56:25 +0200

    Started the JS code

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,8 @@
+function displayTime(event) {
+  if (event.target.value.length > 0) {
+    alert("hello!");
+  }
+}
 
+let cityChoice = document.querySelector("#city-choice");
+cityChoice.addEventListener("change", displayTime);
